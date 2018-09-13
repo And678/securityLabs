49f024162a7cd7f1fe39202c8e6bca96d9ae6142
andriy kaminskyy <kaminskyy.andriy@gmail.com>
2018-09-13 12:29:06 +0300

    Finished lab2

--- lab2/main.go
+++ lab2/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"io/ioutil"
+	"encoding/hex"
 )
 func addExtension(input []byte) []byte {
 	input = append(input, 0x80)
@@ -26,17 +27,39 @@ func main() {
 		println("Usage: md5 [file] input")
 	} else if len(os.Args) == 2 {
 		data := []byte(os.Args[1])
+		fmt.Printf("%x\n\n", data)
 		h := NewMd5Hash()
 		h.message = addLength(addExtension(data), uint64(len(data)) << 3)
-	} else if len(os.Args) > 2 && os.Args[1] == "file" {
+		fmt.Printf("%x\n\n", h.Sum())
+	} else if len(os.Args) == 3 && os.Args[1] == "file" {
 		data, err := ioutil.ReadFile(os.Args[2])
 		if err != nil {
 			panic(err)
 		}
+		data = data[:len(data) - 1]
 		h := NewMd5Hash()
 		h.message = addLength(addExtension(data), uint64(len(data)) << 3)
 		fmt.Printf("%x\n\n", h.Sum())
+	} else if len(os.Args) == 4 && os.Args[1] == "checkfile" {
+		dataFile, err := ioutil.ReadFile(os.Args[2])
+		dataFile = dataFile[:len(dataFile) - 1]
+		if err != nil {
+			panic(err)
+		}
+		h := NewMd5Hash()
+		h.message = addLength(addExtension(dataFile), uint64(len(dataFile)) << 3)
+		hashSum := h.Sum()
 
+		hashFile, err := ioutil.ReadFile(os.Args[3])
+		if err != nil {
+			panic(err)
+		}
+
+		if string(hashFile[: len(hashFile) - 1]) == hex.EncodeToString(hashSum[:]) {
+			println("File and hash match!")
+		} else {
+			println("File and hash don't match!")
+		}
 	}
 }
 
